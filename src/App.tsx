b33683762478528e5fb4ff6fe663507dc1c0d69f
hisamitsuryouhei <koron.take@gmail.com>
2019-06-30 17:14:27 +0900

    fix : 一旦完成

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import * as tf from "@tensorflow/tfjs";
 import SignaturePad from "react-signature-pad-wrapper";
 
 const App: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
   const [model, setModel] = useState(null);
   const [signaturePad, setSignaturePad] = useState(null);
+  const [maxAccuracy, setMaxAccuracy] = useState("-");
 
   useEffect(() => {
     tf.loadLayersModel(
       "https://raw.githubusercontent.com/tsu-nera/tfjs-mnist-study/master/model/model.json"
     ).then(model => {
-      setIsLoading(false);
       setModel(model);
     });
   }, []);
@@ -51,11 +50,8 @@ const App: React.FC = () => {
   const getAccuracyScores = imageData => {
     return tf.tidy(() => {
       const channels = 1;
-
       let input = tf.browser.fromPixels(imageData, channels);
-
       input = tf.cast(input, "float32").div(tf.scalar(255));
-
       input = input.expandDims();
 
       return model.predict(input).dataSync();
@@ -69,33 +65,47 @@ const App: React.FC = () => {
         const maxAccuracy = accuracyScores.indexOf(
           Math.max.apply(null, accuracyScores)
         );
-        console.log(maxAccuracy);
+        setMaxAccuracy(maxAccuracy);
       });
+  };
+
+  const reset = () => {
+    signaturePad.instance.clear();
+    setMaxAccuracy("-");
   };
 
   return (
     <div className="container">
-      <h1 className="title">MNIST recognition with TensorFlow.js</h1>
-      <SignaturePad
-        width={280}
-        height={280}
-        options={{
-          minWidth: 6,
-          maxWidth: 6,
-          penColor: "white",
-          backgroundColor: "black"
-        }}
-        ref={ref => setSignaturePad(ref)}
-      />
-      <div className="field is-grouped">
-        <p className="control">
-          <a className="button is-primary" onClick={predict}>
-            Predict
-          </a>
-        </p>
-        <p className="control">
-          <a className="button is-light">Reset</a>
-        </p>
+      <h1 className="title has-text-centered">
+        MNIST recognition with TensorFlow.js
+      </h1>
+      <div className="columns is-centered">
+        <div className="column is-3">
+          <SignaturePad
+            width={280}
+            height={280}
+            options={{
+              minWidth: 6,
+              maxWidth: 6,
+              penColor: "white",
+              backgroundColor: "black"
+            }}
+            ref={ref => setSignaturePad(ref)}
+          />
+          <div className="field is-grouped">
+            <p className="control">
+              <a className="button is-primary" onClick={predict}>
+                Predict
+              </a>
+            </p>
+            <p className="control">
+              <a className="button is-light" onClick={reset}>
+                Reset
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="column is-3">推定結果：{maxAccuracy}</div>
       </div>
     </div>
   );
